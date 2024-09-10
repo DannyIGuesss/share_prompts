d0f8f8edf6a6011438f6996770fc6640b0494243
DannyIGuesss <danielgarcia448353@gmail.com>
2024-09-10 11:47:41 -0700

    day 2

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -7,3 +7,23 @@
 //5. **DELETE**: Removes a specific resource from the server.
 //6. **HEAD**: Retrieves the headers of a resource without fetching its body.
 //7. **OPTIONS**: Retrieves the supported HTTP methods and other communication options for a resource
+
+// this would be the express way of doing it. you would do this with all CRUD operations
+export async function GET(request) {
+  return new Response("Hello, Next.js!");
+}
+
+// for next you would do something like this
+// endpoint would look like http://localhost:3000/api/users
+export async function GET(request) {
+  // Handle GET request for /api/users
+  // Retrieve users from the database or any data source
+  const users = [
+    { id: 1, name: "Nancy" },
+    { id: 2, name: "Emily" },
+    { id: 2, name: "Danny" },
+  ];
+
+  // Send the users as a response
+  return new Response(JSON.stringify(users));
+}
